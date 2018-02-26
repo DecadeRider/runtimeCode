b3942e360bee696c94a0eefe7e68c5e31fcd04ae
黎永杰 <liyongjie@liyongjiedembp3.lan>
2018-02-26 21:02:46 +0800

    Initial Commit

---
+++ runtimeCode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  runtimeCode
+//
+//  Created by 黎永杰 on 2018/2/26.
+//  Copyright © 2018年 黎永杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ runtimeCode/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  runtimeCode
+//
+//  Created by 黎永杰 on 2018/2/26.
+//  Copyright © 2018年 黎永杰. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
